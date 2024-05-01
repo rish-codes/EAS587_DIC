024c7cf0862e54bfec8a68ce4f3f42809f5b9539
rish-codes <harinrishabh99@gmail.com>
2024-05-01 13:28:27 -0400

    Modified part1

--- Homework2/part1.py
+++ Homework2/part1.py
@@ -1,25 +1,29 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.ml.feature import StopWordsRemover
 
-conf = SparkConf().setAppName("WordCount")
+conf = SparkConf().setAppName("WordCount").setMaster("local")
 sc = SparkContext(conf=conf)
 
 # Read input text files
 file1_lines = sc.textFile("the_prophet.txt")
 file2_lines = sc.textFile("war_and_peace.txt")
 
-# Combine lines from both files
-combined_lines = file1_lines.union(file2_lines)
-words = combined_lines.flatMap(lambda line: line.lower().split())
-
+# Process first book
+words_file1 = file1_lines.flatMap(lambda line: line.lower().split())
 stop_words = set(StopWordsRemover.loadDefaultStopWords("english"))
-filtered_words = words.filter(lambda word: word not in stop_words)
+filtered_words_file1 = words_file1.filter(lambda word: word not in stop_words)
+word_counts_file1 = filtered_words_file1.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-word_counts = filtered_words.map(lambda word: (word, 1))
-word_counts = word_counts.reduceByKey(lambda a, b: a + b)
+# Process second book
+words_file2 = file2_lines.flatMap(lambda line: line.lower().split())
+filtered_words_file2 = words_file2.filter(lambda word: word not in stop_words)
+word_counts_file2 = filtered_words_file2.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-# Sort the word counts by count in descending order
-sorted_word_counts = word_counts.sortBy(lambda x: x[1], ascending=False)
+# Combine word counts from both books
+combined_word_counts = word_counts_file1.union(word_counts_file2).reduceByKey(lambda a, b: a + b)
+
+# Sort the combined word counts by count in descending order
+sorted_word_counts = combined_word_counts.sortBy(lambda x: x[1], ascending=False)
 
 sorted_word_counts_list = sorted_word_counts.collect()
 
@@ -27,5 +31,7 @@ sorted_word_counts_list = sorted_word_counts.collect()
 with open("output_1.txt", "w") as f:
     for word, count in sorted_word_counts_list:
         f.write(f"{word}: {count}\n")
+
+input("Press Enter to terminate:")
 
 sc.stop()
